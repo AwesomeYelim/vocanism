c90015d989dac322a1b96eb40747df6ea57478cc
AwesomeYelim <uiop01900@gmail.com>
2023-12-28 22:23:58 +0900

    css

--- 2024-bible-lection/app/components/lection-button.tsx
+++ 2024-bible-lection/app/components/lection-button.tsx
@@ -11,9 +11,95 @@ interface Props {
 export const LectionButton = ({ words }: Props): JSX.Element => {
   const [modal, setModal] = useState(false)
   const target = words[randomIndex(words.length)]
-
+  const images = ['kakao', 'url', 'imgdown', 'instagram']
   return (
     <>
+      <div className="letter">
+        <Image
+          className="letter-back"
+          src={`/images/letter-back.svg`}
+          alt="back"
+          width={530}
+          height={132}
+          loading="eager"
+          priority
+        />
+        {modal && (
+          <>
+            <div className="modal">
+              <Image
+                className="line"
+                src={`/images/line.svg`}
+                alt="line"
+                width={76}
+                height={104}
+                loading="eager"
+                priority
+              />
+              <Image
+                className="top"
+                src={`/images/top_img.svg`}
+                alt="top"
+                width={110}
+                height={110}
+                loading="eager"
+                priority
+              />
+              <div className="text_area">
+                <p>{target.description}</p>
+                <p>{target.from.replace(/[\(\)]/g, '')}</p>
+              </div>
+              <div className="sns_icon">
+                {images.map((el) => {
+                  return (
+                    <Image
+                      key={el}
+                      className={el}
+                      src={`/images/sns/${el}.svg`}
+                      alt={el}
+                      width={26}
+                      height={25}
+                      loading="eager"
+                      priority
+                    />
+                  )
+                })}
+              </div>
+              <Image
+                className="bottom"
+                src={`/images/bottom_img.svg`}
+                alt="bottom"
+                width={76}
+                height={104}
+                loading="eager"
+                priority
+              />
+            </div>
+          </>
+        )}
+        <Image
+          className="letter-after"
+          src={`/images/letter-after.svg`}
+          alt="after"
+          width={530}
+          height={260}
+          loading="eager"
+          priority
+        />
+        <Image
+          className="letter-cover"
+          src={`/images/letter-cover.svg`}
+          alt="cover"
+          width={530}
+          height={186}
+          loading="eager"
+          priority
+          style={{
+            transformOrigin: 'top center',
+            transform: modal ? 'rotateX(-180deg)' : '',
+          }}
+        />
+      </div>
       <button
         className="lection-button"
         onClick={() => {
@@ -22,47 +108,6 @@ export const LectionButton = ({ words }: Props): JSX.Element => {
       >
         2024 내게 주신 하나님의 말씀
       </button>
-      <div className="letter">
-        <Image
-          className="letter-back"
-          src={`/images/letter-back.svg`}
-          alt="back"
-          width={500}
-          height={500}
-          loading="eager"
-          priority
-        />
-        {modal && (
-          <>
-            <div className="modal">
-              {target.description}
-              <br />
-              {target.from.replace(/[\(\)]/g, '')}
-            </div>
-          </>
-        )}
-        <Image
-          className="letter-after"
-          src={`/images/letter-after.svg`}
-          alt="after"
-          width={500}
-          height={500}
-          loading="eager"
-          priority
-        />
-        <Image
-          className="letter-cover"
-          src={`/images/letter-cover.svg`}
-          alt="cover"
-          width={500}
-          height={500}
-          loading="eager"
-          priority
-          style={{
-            transform: modal ? 'translateY( -30%) rotateX(-180deg)' : '',
-          }}
-        />
-      </div>
     </>
   )
 }
